5501846c3c67b1f56a86a725eee0884932182a82
Lucas Campos <bhlucascampos@gmail.com>
2023-12-23 03:31:33 -0300

    end project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,17 @@ import React from "react"
 import "./Button.css"
 
 
-export default props => 
-  <button className="button">{props.label}</button>
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>{
+  let classes = 'button '
+  classes += props.operation ? 'operation' : ''
+  classes += props.double ? 'double' : ''
+  classes += props.triple ? 'triple' : ''
+  return (
+    <button 
+      onClick={e => props.click(props.label)}
+      className={classes}>
+      {props.label}
+    </button>
+  )
+}
